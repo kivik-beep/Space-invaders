d7dd691526caeb20ab6cb991af0c98eb29108834
K.Kivi <72075784+kivik-beep@users.noreply.github.com>
2020-12-15 23:34:22 +0200

    muotoilua

--- SpaceInvaders/src/main/java/spaceinvaders/ui/End.java
+++ SpaceInvaders/src/main/java/spaceinvaders/ui/End.java
@@ -5,26 +5,51 @@
  */
 package spaceinvaders.ui;
 
-import javafx.event.ActionEvent;
+import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 /**
  * Scene for high scores. 
  * 
  * @author kxkivi
  */
-public class End {
+public class End extends Application {
 
     Scene getScene() {
+        
+        TextField name = new TextField("yourName");
+        Button startNew = new Button("new game");
+        Label highScores = new Label("See your score:");
+        Label first = new Label("1.:");
+        Label second = new Label("2. :");
+        Label third = new Label ("3. : ");
+        
         BorderPane board = new BorderPane();
-        Scene scene = new Scene(board, 500, 500);
+        
+        VBox score = new VBox();
+        score.setSpacing(20);
+        score.getChildren().addAll(highScores, first, second, third);
+       
+        board.setTop(startNew);
+        board.setCenter(score);
+        board.setBottom(name);
+        
+        board.setPrefSize(500, 500);
+        board.setPadding(new Insets(40, 40, 40, 40));
+                
+        
+        Scene scene = new Scene(board);
 
         return scene;
     
@@ -34,4 +59,9 @@ public class End {
     // player can get to start-scene
     
     // High score list shown - adds player 
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
